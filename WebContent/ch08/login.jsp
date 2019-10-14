f3bf86dead79a171563b343ecde1ad868cad625f
프로젝트 공간 <mu_2375@naver.com>
2019-10-15 06:24:30 +0900

    1015-1
    p209

---
+++ WebContent/ch08/login.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<%
+	String id = (String)session.getAttribute("idKey");
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+
+</body>
+</html>
--- src/ch08/getServlet.java
+++ src/ch08/getServlet.java
@@ -1,6 +1,8 @@
 package ch08;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,15 @@ public class getServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String msg = request.getParameter("msg");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Get Servlet ¹æ½Ä</h1>");
+		out.println("<h2>msg : " + msg + "</h2>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
