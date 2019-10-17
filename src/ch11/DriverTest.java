71508a920879fe8d4aecb793358eb0e12a229e61
프로젝트 공간 <mu_2375@naver.com>
2019-10-18 06:39:18 +0900

    1018-1
    p278

---
+++ src/ch11/DriverTest.java
@@ -0,0 +1,18 @@
+package ch11;
+import java.sql.*;
+
+public class DriverTest {
+
+	public static void main(String[] args) {
+			Connection con;
+			
+			try {
+				Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","swo0836@@");
+				System.out.println("Success");
+			}
+			catch(SQLException ex){System.out.println("SQLException: " + ex);}
+			catch(Exception ex) {System.out.println("Exception: " + ex);}
+	}
+
+}
